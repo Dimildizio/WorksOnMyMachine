e47dfa801df3a0f67bac3af67203577d25ba82a1
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-07-01 02:35:07 +0800

    Edit fastapi response

--- app/data_preprocessing.py
+++ app/data_preprocessing.py
@@ -11,19 +11,21 @@ def process(title: str, name: str, surname: str, sex: str, pclass: int, age: int
                               'Embarked': [embarked], 'Fare': [fare], 'Ticket': [ticket], 'Age': [age],
                               'SibSp': [spouse+siblings], 'Parch': [relatives], 'Pclass': [pclass]})
 
-    process_text(fullname, sex, pclass, age, cabin, fare, siblings, spouse, relatives, embarked, ticket)
+    text = process_text(fullname, sex, pclass, age, cabin, fare, siblings, spouse, relatives, embarked, ticket)
     encoded_df = load_encoder(to_encode)
-    return get_model_result(encoded_df)
+    survived = get_model_result(encoded_df)
+    return survived, text
 
 
 def process_text(fullname: str, sex: str, pclass: int, age: int, cabin: str, fare: float, siblings: int,
                  spouse: int, relatives: int, embarked: str, ticket: str):
     port = {'S': 'Southampton', 'C': 'Cherbourg', 'Q': 'Queenstown'}
     txt = (f'You are {fullname}, a {age} year old {sex}, {pclass} class passenger in cabin {cabin}.\n'
-           f'You got on Titanic at {port[embarked]} for {fare} pounds with {"a spouse " if spouse else ""}'
-           f'{siblings if siblings else ""}{" siblings " if siblings else ""}{relatives if relatives else ""}'
-           f'{" relatives" if siblings else ""}.\nYour ticket is number {ticket}')
+           f'You got on Titanic at {port[embarked]} for {fare} pounds {"with " if any([spouse, siblings, relatives]) else ""} '
+           f'{"a spouse " if spouse else ""} {siblings if siblings else ""}{" siblings " if siblings else ""}'
+           f'{relatives if relatives else ""}{" relatives" if siblings else ""}.\n Your ticket is number {ticket}')
     print(txt)
+    return txt
 
 
 def get_model_result(*args):
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from data_preprocessing import process
+from fastapi import FastAPI, Query
+from data_preprocessing import process, process_text
 
 # from pydantic import BaseModel
 
@@ -14,13 +14,25 @@ def read_root():
 
 @app.get("/prediction")
 # line should work like this: http://localhost:8000/prediction?title=Mr&surname=Johnes&name=John&sex=male&pclass=2&age=30&cabin=E3213&fare=76.4&siblings=1&spouse=0&relatives=1&embarked=S&ticket=121441
-def make_prediction(title: str, name: str, surname: str, sex: str, pclass: int, age: int, cabin: str, fare: float,
-                    siblings: int, spouse: int, relatives: int, embarked: str, ticket: str):
+async def make_prediction(title: str = Query(default='Mr', description="Passenger's title"),
+                          name: str = Query(default='Jack', description="Passenger's name"),
+                          surname: str = Query(default='Dawson', description="Passenger's surname"),
+                          sex: str = Query(default='male', description="Passenger's gender"),
+                          pclass: int = Query(default=1, description="Passenger's class"),
+                          age: int = Query(default=20, description="Passenger's age"),
+                          cabin: str = Query(default='G60', description="Code-number of Passenger's cabin"),
+                          fare: float = Query(default=7, description="Price for the group ticket"),
+                          siblings: int = Query(default=0, description="How many Passenger's siblings are with them"),
+                          spouse: int = Query(default=0, description="Passenger's partner"),
+                          relatives: int = Query(default=0, description="How many Passenger's children are onboard"),
+                          embarked: str = Query(default='S', description="Port where the Passenger got on the ship"),
+                          ticket: str = Query(default='6666', description="Passenger's ticket number")):
 
-    output = process(title, name, surname, sex, pclass, age, cabin, fare, siblings, spouse, relatives, embarked, ticket)
+    output, text = process(title, name, surname, sex, pclass, age, cabin, fare, siblings, spouse, relatives, embarked, ticket)
     print('The model predicted:', output)
-    if output:
-        result = f': {name} has survived'
-    else:
-        result = f': {name} has died'
-    return {"Prediction": str(output)+result}
+
+    response = {'Message': text}
+    result = 'Congratulations! You have survived the Titanic catastrophy' if output else "Congratulations! " \
+             "You are now in a better world! You have died in a Titanic catastrophy"
+    response['Prediction'] = f'\n[{str(output)}] - {result}'
+    return response
