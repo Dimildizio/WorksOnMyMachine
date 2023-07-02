165b4fc60df6c5591fe81e500614ffd359b98a39
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-07-02 08:26:07 +0800

    Make cohesive fastapi, flask and docker files

--- app/flask_app/app.py
+++ app/flask_app/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
 import requests
+import os
+from flask import Flask, request, render_template
 from random import randint
 
 
@@ -11,23 +12,26 @@ def parameter_selection():
     return render_template('parameter_selection.html')
 
 
-@app.route('/prediction', methods=['GET'])
+@app.route('/prediction', methods=['POST', "GET"])
 def prediction():
     title = request.args.get('title')
     name = request.args.get('name')
     surname = request.args.get('surname')
     sex = request.args.get('sex')
-    age = request.args.get('age')
-    sibs = request.args.get('siblings')
-    spouse = request.args.get('spouse')
-    relatives = request.args.get('relatives')
-    pclass = request.args.get('pclass')
+    age = int(request.args.get('age'))
+    sibs = int(request.args.get('siblings'))
+    spouse = int(request.args.get('spouse'))
+    relatives = int(request.args.get('relatives'))
+    pclass = int(request.args.get('pclass'))
     embarked = request.args.get('embarked')
     deck = request.args.get('deck')
     fare = 13.61
     ticket = randint(1, 3000)
 
-    url = 'http://localhost:8000/prediction'
+    if 'DOCKER_ENV' in os.environ:
+        url = 'http://worksonmymachine-backend-1:8000/prediction'   # If ran through docker
+    else:
+        url = 'http://localhost:8000/prediction'  # If direct run without docker
     params = {
         'title': title,
         'name': name,
@@ -42,15 +46,19 @@ def prediction():
         'relatives': relatives,
         'embarked': embarked,
         'ticket': ticket}
+
     response = requests.get(url, params=params)
 
     result = response.json()
     message = result['Message']
     fate = result['Prediction']
+    print("Rendering predicted_fate.html template")
 
     return render_template('predicted_fate.html', message=message, fate=fate)
 
 
 if __name__ == '__main__':
     print('I am running')
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
+
+
