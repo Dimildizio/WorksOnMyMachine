d7675e0f27e7f5add85ff5594fe2916fd4577a1f
Dimildizio <42382713+Dimildizio@users.noreply.github.com>
2023-07-07 22:47:57 +0800

    Add an easter egg

--- app/flask_app/app.py
+++ app/flask_app/app.py
@@ -28,6 +28,12 @@ def prediction():
     fare = 13.61
     ticket = randint(1, 3000)
 
+    if sex == 'male' and dicaprio(name, surname):
+        message = f"Sorry, {name} {surname} there isn't enough space on that door"
+        # Change for other html page
+        page = render_template('leo.html', message=message)
+        return page
+
     if 'DOCKER_ENV' in os.environ:
         url = 'http://worksonmymachine-backend-1:8000/prediction'   # If ran through docker
     else:
@@ -52,13 +58,25 @@ def prediction():
     result = response.json()
     message = result['Message']
     fate = result['Prediction']
-    print("Rendering predicted_fate.html template")
+    print("Rendering predicted fate template")
 
-    return render_template('predicted_fate.html', message=message, fate=fate)
+    if fate[0]:
+        # Survived page
+        page = render_template('predicted_fate.html', message=message, fate=fate)
+    else:
+        # Change later for death page
+        page = render_template('predicted_fate.html', message=message, fate=fate)
+    return page
+
+
+def dicaprio(name, surname):
+    jack = name in ['Jack', 'Джек']
+    dawson = surname in ['Dawson', 'Доусон', 'Довсон', 'Даусон']
+    leo = name in ['Leo', 'Leonardo', 'Леонардо', 'Лео']
+    di = ''.join(surname.split()).lower() == 'dicaprio'
+    return (leo and di) or (jack and dawson)
 
 
 if __name__ == '__main__':
     print('I am running')
     app.run()
-
-
